04eec28ca63327d5cce59665a7cc322dcfe9955b
taydekaran <karantayade29@gmail.com>
2025-10-18 01:08:41 +0530

    largest number in three  number using "import kotlin.math.max" , maxOf()

--- src/sum_of_two_number.kt
+++ src/sum_of_two_number.kt
@@ -6,4 +6,19 @@ fun main(){
     val second  = readln().toInt()
 //    return first + second    //we can not use this return statement  for "main"
     println("sum is ${first + second} ")
+
+     largnum()
 }
+
+fun largnum (){
+    println("Enter first number : ")
+    val first = readln().toInt()
+    println("Enter second number : ")
+    val second = readln().toInt()
+    println("Enter third number : ")
+    val third = readln().toInt()
+
+    val max = maxOf(first , second, third)
+    print("number is greater  : $max")
+}
+
