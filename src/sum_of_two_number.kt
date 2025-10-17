f73e37d651edc49ff9b778d390dfe6bea56956cf
taydekaran <karantayade29@gmail.com>
2025-10-18 00:31:53 +0530

    sum in of two number

---
+++ src/sum_of_two_number.kt
@@ -0,0 +1,9 @@
+//sum of two number
+fun main(){
+    println("Enter first number : ")// interpolation
+    val first = readln().toInt()
+    println("Enter second number : ")// interpolation
+    val second  = readln().toInt()
+//    return first + second    //we can not use this return statement  for "main"
+    println("sum is ${first + second} ")
+}
