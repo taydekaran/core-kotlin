3fbb979373ff0c359967b2d4a729c0e81bdec59d
taydekaran <karantayade29@gmail.com>
2025-10-18 01:28:03 +0530

    check number is positive negative or zero

--- src/sum_of_two_number.kt
+++ src/sum_of_two_number.kt
@@ -1,5 +1,11 @@
 //sum of two number
 fun main(){
+    NumType()
+    SunOfTwoNum()
+     largnum()
+}
+
+fun SunOfTwoNum(){
     println("Enter first number : ")// interpolation
     val first = readln().toInt()
     println("Enter second number : ")// interpolation
@@ -7,7 +13,6 @@ fun main(){
 //    return first + second    //we can not use this return statement  for "main"
     println("sum is ${first + second} ")
 
-     largnum()
 }
 
 fun largnum (){
@@ -21,4 +26,14 @@ fun largnum (){
     val max = maxOf(first , second, third)
     print("number is greater  : $max")
 }
+//check number is positive negative or zero
 
+  fun NumType(){
+      println("Enter Number :")
+      val input = readlnOrNull()?.toInt()  // it can be null
+      //check  input is null
+      if(input != null){
+          if(input > 0) println("number is positive") ; if (input < 0) println("number is negative") ; if (input == 0) println("input is zero")
+
+      }
+  }
